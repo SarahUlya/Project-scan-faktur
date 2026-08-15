a92a263d2ae12f4f4f8bcee2963ce81e19ac1030
Sarah Ulya <sarahsamodera@gmail.com>
2026-08-15 10:42:47 +0700

    update API URL to HTTPS

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://103.117.56.138:3000/api/v1/",
-  headers: {
-    "ngrok-skip-browser-warning": "true",
-  },
+  baseURL: "https://api.reysprime.my.id/api/v1/",
+
+  
 });
 
 axiosInstance.interceptors.request.use(
